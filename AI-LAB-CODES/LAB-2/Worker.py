ae94f2407b1d7d97f720ab5cf35fda2581422860
Muhammad Usman Karim <96872170+usmannkarim@users.noreply.github.com>
2023-03-21 00:03:52 +0500

    Update Worker.py

--- AI-LAB-CODES/LAB-2/Worker.py
+++ AI-LAB-CODES/LAB-2/Worker.py
@@ -3,11 +3,11 @@ def workerEfficiency(Time):
     if Time <= 3:
         print(f"Time={Time} hour's, Good Job Keep up the good work")
     elif Time > 3 and Time <= 4:
-        print(f"Time={Time} hour's, You need to improve your spee")
+        print(f"Time={Time} hour's, You need to improve your speed")
     elif Time > 4 and Time <= 5:
-        print(f"Time={Time} hour's, You will have to go through the training to improve your speed")
+        print(f"Time={Time} hour's, You'll have to go through the training to improve your speed")
     elif Time > 5:
-        print(f"Time={Time} hour's, You are Fired")
+        print(f"Time={Time} hour's, You are Fired!")
     else:
         print("Error")
     
